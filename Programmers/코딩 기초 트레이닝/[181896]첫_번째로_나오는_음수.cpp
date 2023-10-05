255787aa407d567f5604d3649896191ae6ae1744
krydyh <kryd_@naver.com>
2023-10-05 11:54:08 +0900

    Create [181896]첫_번째로_나오는_음수.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181896]첫_번째로_나오는_음수.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    
+    for(int i = 0; i < num_list.size(); ++i) {
+        if(num_list[i] < 0) return i;
+    }
+    return answer;
+}
