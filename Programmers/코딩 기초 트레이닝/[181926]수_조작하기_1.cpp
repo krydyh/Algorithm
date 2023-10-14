3dc93882921d20700641a4512d12bde0441624c5
krydyh <kryd_@naver.com>
2023-10-14 23:57:28 +0900

    Create [181926]수_조작하기_1.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181926]수_조작하기_1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    
+    for(int i = 0; i < control.size(); ++i) {
+        if(control[i] == 'w') answer += 1;
+        else if(control[i] == 's') answer -= 1;
+        else if(control[i] == 'd') answer += 10;
+        else if(control[i] == 'a') answer -= 10;
+    }
+    return answer;
+}
