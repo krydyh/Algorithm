5fdb12c5ca625830ccb74bfc14cfffc9c0c39b3b
krydyh <kryd_@naver.com>
2023-10-18 13:23:47 +0900

    Create [181873]특정한_문자를_대문자로_바꾸기.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181873]특정한_문자를_대문자로_바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string, string alp) {
+    string answer = "";
+    
+    for(int i = 0; i < my_string.size(); ++i) {
+        if(my_string[i] == alp[0]) answer += (my_string[i] - 32);
+        else answer += my_string[i];
+    }
+    return answer;
+}
