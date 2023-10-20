0a2f6cd169b987c815586d52a2ce794d9b3a9804
krydyh <kryd_@naver.com>
2023-10-20 13:00:51 +0900

    Create [181935]홀짝에_따라_다른_값_반환하기.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181935]홀짝에_따라_다른_값_반환하기.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    if(n % 2 == 1) {
+        for(int i = 1; i <= n; ++i) {
+            if(i % 2 == 1) answer += i;
+        }
+    }
+    else {
+        for(int i = 2; i <= n; ++i) {
+            if(i % 2 == 0 ) answer += i * i;
+        }
+    }
+    return answer;
+}
