d0f3355d12ccd147025139ff084684920a60079a
krydyh <kryd_@naver.com>
2023-10-22 18:07:28 +0900

    Create [181944]홀짝_구분하기.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181944]홀짝_구분하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    
+    if(n % 2 == 1) cout << n << " is odd";
+    else cout << n << " is even";
+    return 0;
+}
