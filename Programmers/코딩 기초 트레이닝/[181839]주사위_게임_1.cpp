cdac12794fe1dfaf215d5ce84178adca9e5ba14b
krydyh <kryd_@naver.com>
2023-10-30 22:16:42 +0900

    Create [181839]주사위_게임_1.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181839]주사위_게임_1.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(a % 2 == 1 && b % 2 == 1) answer = (a * a) + (b * b);
+    else if((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) answer = 2 * (a + b);
+    else answer = abs(a - b);
+    return answer;
+}
