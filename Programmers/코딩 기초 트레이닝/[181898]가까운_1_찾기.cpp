f9eea6290cbf035f3aa8e3e2b2bbded004352dcb
krydyh <kryd_@naver.com>
2023-11-14 18:30:35 +0900

    Create [181898]가까운_1_찾기.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181898]가까운_1_찾기.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    for(int i = idx; i < arr.size(); ++i) {
+        if(arr[i] == 1) return i;
+    }
+    return -1;
+}
