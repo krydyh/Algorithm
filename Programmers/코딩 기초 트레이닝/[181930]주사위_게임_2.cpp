8346d0b93cff91ad96e90c4e01ed1b1f3aa8aad2
krydyh <kryd_@naver.com>
2023-11-19 19:55:42 +0900

    Create [181930]주사위_게임_2.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181930]주사위_게임_2.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    
+    if(a != b && a != c && b != c) answer = a + b + c;
+    else if((a == b && a != c) || (a == c && a != b) || (b == c && a != b)) answer = (a + b + c) * (pow(a, 2) + pow(b, 2) + pow(c, 2));
+    else if(a == b && b == c) answer = (a + b + c) * (pow(a, 2) + pow(b, 2) + pow(c, 2)) * (pow(a, 3) + pow(b, 3) + pow(c, 3));
+    return answer;
+}
