185e65959ed7b51326db33a6b61d8aaa9ff7c543
krydyh <kryd_@naver.com>
2023-12-07 21:33:57 +0900

    Create [181918]배열_만들기_4.cpp

---
+++ Programmers/코딩 기초 트레이닝/[181918]배열_만들기_4.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i = 0;
+    
+    while(i < arr.size()) {
+        if(stk.empty()) {
+            stk.push_back(arr[i]);
+            i++;
+        }
+        else if(stk.empty() == 0 && stk[stk.size() - 1] < arr[i]) {
+            stk.push_back(arr[i]);
+            i++;
+        }
+        else if(stk.empty() == 0 && stk[stk.size() - 1] >= arr[i]) {
+            stk.pop_back();
+        }
+    }
+    return stk;
+}
