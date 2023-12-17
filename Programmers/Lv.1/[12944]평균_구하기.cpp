2e6756a9b6472869e80d04bbeb7905982077c4e0
krydyh <kryd_@naver.com>
2023-12-17 14:42:22 +0900

    Create [12944]평균_구하기.cpp

---
+++ Programmers/Lv.1/[12944]평균_구하기.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    for(int i = 0 ; i < arr.size(); ++i) answer += arr[i];
+    return answer /= arr.size();
+}
