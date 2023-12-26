14b6d84990a287bfd2280eb8432def91923038b4
krydyh <kryd_@naver.com>
2023-12-26 14:03:49 +0900

    Create [12935]제일_작은_수_제거하기.cpp

---
+++ Programmers/Lv.1/[12935]제일_작은_수_제거하기.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    arr.erase(min_element(arr.begin(), arr.end()));
+    if(arr.empty()) arr.push_back(-1);
+    return arr;
+}
