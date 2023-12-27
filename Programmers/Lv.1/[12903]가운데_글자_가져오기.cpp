e6a1c5bc378eff1e9b60a38e8bc5cdcc692abdb9
krydyh <kryd_@naver.com>
2023-12-27 16:31:27 +0900

    Create [12903]가운데_글자_가져오기.cpp

---
+++ Programmers/Lv.1/[12903]가운데_글자_가져오기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    if(s.size() % 2 == 1) answer += s[s.size() / 2];
+    else {
+        answer += s[s.size() / 2 - 1];
+        answer += s[s.size() / 2];
+    }
+    return answer;
+}
