df7821d4419921836b219f1600400474a4b71d2a
krydyh <kryd_@naver.com>
2023-12-28 13:35:25 +0900

    Create [12922]수박수박수박수박수박수?.cpp

---
+++ Programmers/Lv.1/[12922]수박수박수박수박수박수?.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    for(int i = 0; i < n / 2; ++i) {
+        answer += "수박";
+    }
+    if(n % 2 == 1) answer += "수";
+    return answer;
+}
