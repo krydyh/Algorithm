626409b65d8badda6ea510e5f99a5b14256f2023
krydyh <kryd_@naver.com>
2023-12-28 13:36:28 +0900

    Create [77884]약수의_개수와_덧셈.cpp

---
+++ Programmers/Lv.1/[77884]약수의_개수와_덧셈.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for(int i = left; i <= right; ++i) {
+        int count = 1;
+        for(int j = 2; j <= i; ++j) {
+            if(i % j == 0) count++;
+        }
+        if(count % 2 == 0) answer += i;
+        else answer -= i;
+    }
+    return answer;
+}
