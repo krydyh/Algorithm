3c34dee76566a4bd87327bb792fffdea6059ece4
krydyh <kryd_@naver.com>
2023-12-31 22:39:00 +0900

    Create [12950]행렬의_덧셈.cpp

---
+++ Programmers/Lv.1/[12950]행렬의_덧셈.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    for(int i = 0; i < arr1.size(); ++i) {
+        for(int j = 0; j < arr1[0].size(); ++j) {
+            arr1[i][j] += arr2[i][j];
+        }
+    }
+    return arr1;
+}
