fd187092da4b48bc01419abadf4448bfe6516a3e
krydyh <kryd_@naver.com>
2024-01-07 17:12:07 +0900

    Create [12915]문자열_내_마음대로_정렬하기.cpp

---
+++ Programmers/Lv.1/[12915]문자열_내_마음대로_정렬하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    sort(strings.begin(), strings.end(), [n](string a, string b) {
+        if(a[n] == b[n]) return a < b;
+        return a[n] < b[n];
+    });
+    return strings;
+}
