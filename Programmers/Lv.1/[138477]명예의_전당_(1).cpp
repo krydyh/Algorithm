83b5d176285437acac442375d53c96d6f920cef7
krydyh <kryd_@naver.com>
2024-01-16 13:09:24 +0900

    Create [138477]명예의_전당_(1).cpp

---
+++ Programmers/Lv.1/[138477]명예의_전당_(1).cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i = 0; i < score.size(); ++i) {
+        if(pq.size() < k) pq.push(score[i]);
+        else if(score[i] > pq.top()) {
+            pq.pop();
+            pq.push(score[i]);
+        }
+        answer.push_back(pq.top());
+    }
+    return answer;
+}
