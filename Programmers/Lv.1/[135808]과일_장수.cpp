5b65b15ea67383e82f7f121ce71b67a3a5050e89
krydyh <kryd_@naver.com>
2024-01-19 14:58:38 +0900

    Create [135808]과일_장수.cpp

---
+++ Programmers/Lv.1/[135808]과일_장수.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+    sort(score.rbegin(), score.rend());
+    for(int i = m - 1; i < score.size(); i += m) {
+        answer += score[i] * m;
+    }
+    return answer;
+}
